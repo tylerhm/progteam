8d23b3fb35655cdb0dc823ddfb42370025ebff28
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2021-10-13 11:05:00 -0400

    Removed some data

--- learn_sets/flowI/cow.cpp
+++ learn_sets/flowI/cow.cpp
@@ -49,9 +49,57 @@ struct FordFulkerson {
     }
 };
 
+template <class T> int sgn(T x) { return (x > 0) - (x < 0); }
+template<class T>
+struct Point {
+        typedef Point P;
+        T x, y;
+        explicit Point(T x=0, T y=0) : x(x), y(y) {}
+        bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+        bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+        P operator+(P p) const { return P(x+p.x, y+p.y); }
+        P operator-(P p) const { return P(x-p.x, y-p.y); }
+        P operator*(T d) const { return P(x*d, y*d); }
+        P operator/(T d) const { return P(x/d, y/d); }
+        T dot(P p) const { return x*p.x + y*p.y; }
+        T cross(P p) const { return x*p.y - y*p.x; }
+        T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+        T dist2() const { return x*x + y*y; }
+        double dist() const { return sqrt((double)dist2()); }
+        double angle() const { return atan2(y, x); }
+        P unit() const { return *this/dist(); } // makes dist()=1
+        P perp() const { return P(-y, x); } // rotates +90 degrees
+        P normal() const { return perp().unit(); }
+        P rotate(double a) const {
+                return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
+        friend ostream& operator<<(ostream& os, P p) {
+                return os << "(" << p.x << "," << p.y << ")"; }
+};
+
+template<class P> bool onSegment(P s, P e, P p) {
+        return p.cross(s, e) == 0 && (s - p).dot(e - p) <= 0;
+}
+
+template<class P> vector<P> segInter(P a, P b, P c, P d) {
+        auto oa = c.cross(d, a), ob = c.cross(d, b),
+     oc = a.cross(b, c), od = a.cross(b, d);
+        // Checks if intersection is single non-endpoint point.
+        if (sgn(oa) * sgn(ob) < 0 && sgn(oc) * sgn(od) < 0)
+                return {(a * ob - b * oa) / (ob - oa)};
+        set<P> s;
+        if (onSegment(c, d, a)) s.insert(a);
+        if (onSegment(c, d, b)) s.insert(b);
+        if (onSegment(a, b, c)) s.insert(c);
+        if (onSegment(a, b, d)) s.insert(d);
+        return {all(s)};
+}
+
 struct segment {
-    pair<int, int> p1, p2;
-    segment(int x1, int y1, int x2, int y2) : p1(x1, y1), p2(x2, y2) {}
+        Point<int> a, b;
+        segment(Point<int> a, Point<int> b) : a(a), b(b) {}
+        bool inter(segment &o) {
+                return segInter(a, b, o.a, o.b).size();
+        }
 };
 
 int main()
@@ -67,12 +115,14 @@ int main()
         for (int i = 0; i < n; i++)
         {
             int x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
+                        Point<int> one(x1, y1);
+                        Point<int> two(x2, y2);
             // vert
             if (y2 - y1 == 0)
-                horizontal.emplace_back(x1, (y1 < y2)? y1 : y2, x2, (y1 < y2)? y2 : y1);
+                horizontal.emplace_back(one, two);
             // hori
             else
-                vertical.emplace_back((x1 < x2)? x1 : x2, y1, (x1 < x2)? x2 : x1, y2);
+                vertical.emplace_back(one, two);
         }
         FordFulkerson g(n);
         // solve with bipartite matching
@@ -84,12 +134,9 @@ int main()
 
         for (int i = 0; i < horizontal.size(); i++)
             for (int j = 0; j < vertical.size(); j++)
-                if (!(horizontal[i].p2.first < vertical[j].p1.first ||
-                    horizontal[i].p1.first > vertical[j].p1.first ||
-                    horizontal[i].p1.second > vertical[j].p2.second ||
-                    horizontal[i].p1.second < vertical[j].p1.second))
+                                if (horizontal[i].inter(vertical[j]))
                     g.add(i, horizontal.size() + j, 1);
-        
+
         cout << n - g.ff() << "\n";
     }
 
